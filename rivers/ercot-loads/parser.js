6319016d4cd5df67accce2d4ada0b4a97c70584f
Matthew Taylor <matt@numenta.org>
2015-07-09 17:52:44 -0700

    Adding ERCOT Texas energy data.

---
+++ rivers/ercot-loads/parser.js
@@ -0,0 +1,64 @@
+var fs = require('fs')
+  , nodeUrl = require('url')
+  , http = require('http')
+  , request = require('request')
+  , _ = require('lodash')
+  , csvParse = require('csv-parse')
+  , async = require('async')
+  , AdmZip = require('adm-zip')
+  , moment = require('moment-timezone')
+  , cheerio = require('cheerio')
+  ;
+
+function dateStringToTimestampWithZone(dateString, timeString, zone) {
+    // 07/09/2015
+    // 0100
+    var datePieces = dateString.split('/')
+      , timeObject = {}
+      , timestamp
+      ;
+    timeObject.month = parseInt(datePieces.shift()) - 1
+    timeObject.day = parseInt(datePieces.shift())
+    timeObject.year = parseInt(datePieces.shift())
+    timeObject.hour = parseInt(timeString.substr(0,2));
+    timeObject.minute = parseInt(timeString.substr(2,2))
+    timestamp = moment.tz(timeObject, zone).unix();
+    return timestamp;
+}
+
+module.exports = function(config, body, url, temporalDataCallback, metaDataCallback) {
+    var $ = cheerio.load(body)
+      , columnNames = []
+      , id = 'actual_loads_of_weather_zones'
+      ;
+
+    $('#today tr').each(function(i, tr) {
+        var row = []
+          , dateString
+          , timeString
+          , timestamp
+          ;
+
+        $(tr).find('td').each(function(j, td) {
+            var string = $(td).html().trim()
+              , value;
+
+            if (i == 0 || columnNames[j] == 'Oper Day' || columnNames[j] == 'Hour Ending') {
+                value = string;
+            } else {
+                value = parseFloat(string);
+            }
+            row.push(value);
+
+        });
+
+        if (i == 0) {
+            columnNames = row;
+        } else {
+            dateString = row[columnNames.indexOf('Oper Day')];
+            timeString = row[columnNames.indexOf('Hour Ending')];
+            timestamp = dateStringToTimestampWithZone(dateString, timeString, config.timezone);
+            temporalDataCallback(id, timestamp, row);
+        }
+    });
+};
