f0efbfa32c78746be62eb961e7ce9db2af93ba59
Matthew Taylor <matt@numenta.org>
2015-07-19 07:26:01 -0700

    River user test.

--- test-river.js
+++ test-river.js
@@ -1,27 +1,57 @@
 var fs = require('fs');
 var path = require('path');
+var _ = require('lodash');
+var async = require('async');
 var Mocha = require('mocha');
-var riverName = process.argv[2];
 
-if (! riverName) {
-    console.log('You must specify a river name as the only argument.');
-    printUsage();
-    process.exit(-1);
+var riverDir = path.join(__dirname, 'rivers');
+var riverName = process.argv[2];
+var runners = [];
+
+function testRiver(name, cb) {
+    var mocha = new Mocha();
+    console.log('testing river %s', name);
+    global._RIVER_NAME_ = name;
+
+    mocha.addFile(path.join('test', 'rivers', 'river-tests.js'));
+
+    mocha.run(function(failures){
+        process.on('exit', function () {
+            if (cb) return cb(failures);
+            console.log('exiting');
+            process.exit(failures);
+        });
+    });
+
 }
 
-global._RIVER_NAME_ = riverName;
+if (! riverName) {
 
-// First, you need to instantiate a Mocha instance.
-var mocha = new Mocha();
-mocha.addFile(path.join('test', 'rivers', 'river-tests.js'));
+    /*
+     * This part isn't quite done yet. I was planning on writing it so that if
+     * a river name was not specified on the command line, then all the river
+     * tests would run. But I'm running into complications and I'm not sure what
+     * the problem is. 
+     */
 
-// Now, you can run the tests.
-mocha.run(function(failures){
-    process.on('exit', function () {
-        process.exit(failures);
-    });
-});
-
-function printUsage() {
-    console.log('> node test-river.js <river-name>\n');
+    //_.each(fs.readdirSync(riverDir), function(name) {
+    //    if (fs.statSync(path.join(riverDir, name)).isDirectory()) {
+    //        runners.push(function(cb) {
+    //            testRiver(name, cb);
+    //        });
+    //    }
+    //});
+    //console.log(runners);
+    //async.series(runners, function(failures) {
+    //    console.log('test runner done');
+    //    if (failures) {
+    //        console.log('exiting');
+    //        process.exit(failures);
+    //    } else {
+    //        console.log('exiting');
+    //        process.exit();
+    //    }
+    //});
+} else {
+    testRiver(riverName);
 }
